47c88112110554d36fe0bf3495ed032c3acf3587
M-isabella <33769207+M-isabella@users.noreply.github.com>
2017-11-18 23:29:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function getGreeting(){ 
-return new Date();
-}
 window.onload = function() {
 document.getElementById('greeting').innerHTML = getGreeting();
 };
+function getGreeting(){ 
+return new Date();
+}
